e92054f179d24afb94165717bd8bd6aa231427bc
Yan Maia <yandamasceno01@gmail.com>
2022-12-03 02:09:23 -0300

    vararg

--- src/main/kotlin/com/ydmaia/Vararg.kt
+++ src/main/kotlin/com/ydmaia/Vararg.kt
@@ -2,12 +2,12 @@ package com.ydmaia
 
 fun main() {
 
-    fun printAll(vararg messages: String) {                            // 1
-        for (m in messages) println(m)
+    fun printAll(vararg messages: String) {                            // 1 é onde passamos parÇametros do mesmo tipo de quantidades indefinidas
+        for (m in messages) println(m) //pega um array com todas as mensagens, independente da quntidade e imprima
     }
     printAll("Hello", "Hallo", "Salut", "Hola", "你好")                 // 2
 
-    fun printAllWithPrefix(vararg messages: String, prefix: String) {  // 3
+    fun printAllWithPrefix(vararg messages: String, prefix: String) {  //3 Argumentos nomeados
         for (m in messages) println(prefix + m)
     }
     printAllWithPrefix(
@@ -16,7 +16,8 @@ fun main() {
     )
 
     fun log(vararg entries: String) {
-        printAll(*entries)                                             // 5
+        printAll(*entries)                                             // 5 Se estou chamando uma função que tem vararg, para
+                                                                        //que ele não entenda como um array, colocamos o '*'.
     }
-
+    //Importante saber que um vararg em tempo de execução é um array
 }
